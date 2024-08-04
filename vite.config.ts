e2945a203e035bf6e0afee8cc89bad2ffda7d39b
plymer <93949066+plymer@users.noreply.github.com>
2024-08-04 15:36:00 -0600

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/game-hub",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const configs = { plugins: [react()] };
+  if (command === "serve") {
+    // npm run dev AKA development
+    return { ...configs, base: "" };
+  } else {
+    // npm run build AKA production deployment
+    return { ...configs, base: "/game-hub" };
+  }
 });
 
